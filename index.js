5846c7d5343776d65d1c0e63b5ae77fe4875738b
Kaleb Elwert <belak@coded.io>
2020-06-23 23:22:00 -0700

    Export Headers and friends from node-fetch

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = require('node-fetch');
+var fetch = require('node-fetch');
+
+module.exports = {
+  default: fetch,
+  fetch: fetch,
+  Headers: fetch.Headers,
+  Request: fetch.Request,
+  Response: fetch.Response
+};
