bafda7fb1b316d0e394018de4b5d01caf81511f4
Kaleb Elwert <belak@coded.io>
2020-06-23 23:30:56 -0700

    Fix issue with exporting the fetch function in module loaders

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-var fetch = require('node-fetch');
+var nodeFetch = require('node-fetch');
+
+// In some module systems, fetch is exported as default rather than at the root.
+// We catch this and re-export the actual function rather than the module.
+var fetchFn = nodeFetch.default || nodeFetch;
 
 module.exports = {
-  default: fetch,
-  fetch: fetch,
-  Headers: fetch.Headers,
-  Request: fetch.Request,
-  Response: fetch.Response
+  default: fetchFn,
+  fetch: fetchFn,
+  Headers: nodeFetch.Headers,
+  Request: nodeFetch.Request,
+  Response: nodeFetch.Response
 };
